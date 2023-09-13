0f6245843ec4903e4d1cdda4556aa10a0066ccf4
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-09-13 16:54:47 +0530

    add nextjs project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: false, 
+    darkTheme: "dark", 
+    base: true, 
+    styled: true, 
+    utils: true, 
+    rtl: false, 
+    prefix: "",
+    logs: true, 
+  },
 }
 export default config
